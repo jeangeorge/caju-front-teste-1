f729e711261891c2b0306c0bc0b88f20922128ca
Jean George <jean.george.alves@gmail.com>
2024-09-19 01:25:09 -0300

    refactor: rename folder

--- src/pages/Dashboard/components/index.ts
+++ src/pages/Dashboard/components/index.ts
@@ -1,3 +1,3 @@
 export { default as Columns } from "./Columns";
 export { default as RegistrationCard } from "./RegistrationCard";
-export { default as SearchBar } from "./SearchBarTest";
+export { default as SearchBar } from "./SearchBar";
